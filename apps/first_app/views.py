e9d882b9f86e4e1d0fd4bcad56f71ed1e2bd94de
kevinyk <kevinyoungkim@gmail.com>
2018-05-16 16:00:26 -0700

    finish login_reg with manager

--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -12,49 +12,13 @@ def index(request):
 
 def create(request):
     if request.method == "POST":
-        # Original validation logic applies
-        # Name must be present and at least 2 characters long
-        # Description must be present and at least 8 characters
-        # price must be present
-        print(request.POST)
-        # 1. create errors dictionary
-        errors = {}
-        # 2. validate post information
-        if len(request.POST['name']) == 0:
-            errors['name'] = "Name must be present"
-        elif len(request.POST['name'])<2:
-            errors['name'] = "Name must be at least 2 characters long"
-
-        if len(request.POST['price']) == 0:
-            errors['price'] = "Price must be present"
         
-        if len(request.POST['description']) == 0:
-            errors['description'] = "Description is required"
-        elif len(request.POST['description']) < 8:
-            errors['description'] = "Description must be at least 8 characters long"
-
-        # THIS IS NEW FOR REGISTERING
-        if not EMAIL_REGEX.match(request.POST['email']):
-            errors['email'] = "Email must be of a proper format"
-        else:
-            users_with_same_email = Pizza.objects.filter(email=request.POST['email'])
-            if len(users_with_same_email)>0:
-                errors['email'] = 'Email is already taken'
-        if request.POST['password'] != request.POST['password_confirm']:
-            errors['password'] = 'Password fields must match'
-        if len(request.POST['password'])<8:
-            errors['password'] = 'Password must be at least 8 characters long'
-        
-        print(errors)
-        # 3. Check if errors exist, if they do, add them to messages
-        if len(errors):
-            for key,value in errors.items():
+        result = Pizza.objects.validate_registration(request.POST)
+        if 'errors' in result:
+            for key,value in result['errors'].items():
                 messages.error(request, value)
         else:
-            # THIS IS ALSO NEW, WE NEED TO ADD BCRYPT TO HASH PW
-            hashed_pw = bcrypt.hashpw(request.POST['password'].encode(), bcrypt.gensalt())
-            Pizza.objects.create(name = request.POST['name'], price = request.POST['price'], description = request.POST['description'], email = request.POST['email'], password=hashed_pw)
-            messages.success(request, 'successfully created a pizza!')
+            messages.success(request, "Welcome to the club!")
         return redirect('/')
 
 
