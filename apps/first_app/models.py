89b33fbb8a0754fae86787e73dc148148857a96d
kevinyk <kevinyoungkim@gmail.com>
2018-05-16 11:14:56 -0700

    manager version complete

--- apps/first_app/models.py
+++ apps/first_app/models.py
@@ -1,7 +1,41 @@
 from django.db import models
-
 # Create your models here.
+class PizzaManager(models.Manager):
+    def validate_pizza(self, postData):
+        # Original validation logic applies
+        # Name must be present and at least 2 characters long
+        # Description must be present and at least 8 characters
+        # price must be present
+        print(postData)
+        # 2. create errors dictionary
+        errors = {}
+        # 3. validate post information
+        if len(postData['name']) == 0:
+            errors['name'] = "Name must be present"
+        elif len(postData['name'])<2:
+            errors['name'] = "Name must be at least 2 characters long"
 
+        if len(postData['price']) == 0:
+            errors['price'] = "Price must be present"
+        
+        if len(postData['description']) == 0:
+            errors['description'] = "Description is required"
+        elif len(postData['description']) < 8:
+            errors['description'] = "Description must be at least 8 characters long"
+        print(errors)
+        # 4. If errors exist, package them in a dictionary
+        if len(errors):
+            result = {
+                'errors': errors
+            }
+            return result
+        # 5. If no errors, create the pizza here and add to the dictionary
+        else:
+            pizza = self.create(name = postData['name'], description = postData['description'], price = postData['price'])
+            result = {
+                'the_pizza': pizza
+            }
+            return result
 
 class Pizza(models.Model):
     name = models.CharField(max_length = 255)
@@ -9,3 +43,4 @@ class Pizza(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+    objects = PizzaManager()
--- apps/first_app/views.py
+++ apps/first_app/views.py
@@ -5,35 +5,18 @@ from .models import Pizza
 # Create your views here.
 def index(request):
     context = {'all_pizzas': Pizza.objects.all()}
-    return render(request, 'first_app/index.html', context)
+    return render(request, 'first_app/index.html', context )
 
 def create(request):
-    if request.method == "POST":
-        # Original validation logic applies
-        # Name must be present and at least 2 characters long
-        # Description must be present and at least 8 characters
-        # price must be present
-        print(request.POST)
-        # 1. create errors dictionary
-        errors = {}
-        # 2. validate post information
-        if len(request.POST['name']) == 0:
-            errors['name'] = "Name must be present"
-        elif len(request.POST['name'])<2:
-            errors['name'] = "Name must be at least 2 characters long"
 
-        if len(request.POST['price']) == 0:
-            errors['price'] = "Price must be present"
+    if request.method == "POST":
+        # 1. Call the manager function
+        pizza = Pizza.objects.validate_pizza(request.POST)
         
-        if len(request.POST['description']) == 0:
-            errors['description'] = "Description is required"
-        elif len(request.POST['description']) < 8:
-            errors['description'] = "Description must be at least 8 characters long"
-        print(errors)
-        # 3. Check if errors exist, if they do, add them to messages
-        if len(errors):
-            for key,value in errors.items():
+        # 5. Check if errors exist, if they do, add them to messages
+        if 'errors' in pizza:
+            for key,value in pizza['errors'].items():
                 messages.error(request, value)
         else:
-            Pizza.objects.create(name = request.POST['name'], price = request.POST['price'], description = request.POST['description'])
+            messages.success(request, 'YOU DID IT')
         return redirect('/')
